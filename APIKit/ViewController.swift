5e1d7a78a226d9ab7b4ec9315add28b808c57183
kosuge0820 <satoshi082015@gmail.com>
2016-05-09 10:46:50 +0900

    API導入

---
+++ APIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  APIKit
+//
+//  Created by 小菅仁士 on 2016/05/09.
+//  Copyright © 2016年 kosuge satoshi. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+ use_frameworks!
+
+target 'APIKit' do
+ pod 'APIKit', '~> 1.3.0'
+end
+
+target 'APIKitTests' do
+
+end
+
+target 'APIKitUITests' do
+
+end
+
